3b45fee16ef045b191191fab566fc282c70ca12e
Makoto Uwaya <makoto.uwaya@gmail.com>
2017-11-26 17:26:24 +0900

    店舗画像Urlが未指定の場合に代替画像を表示
    - 代替画像はリソースとして組み込む

---
+++ MainApp/Converter/DefaultImageConverter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+using System.Windows.Media.Imaging;
+
+namespace MainApp.Converter
+{
+    public class DefaultImageConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value != null)
+            {
+                return value;
+            }
+
+            return new BitmapImage(new Uri("pack://application:,,,/Resource/ImageNotFound.jpg"));
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
