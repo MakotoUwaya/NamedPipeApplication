03dc194df4a040eb868f6d41bb0215cf8bc93443
Makoto Uwaya <makoto.uwaya@gmail.com>
2017-11-26 23:15:21 +0900

    ページが変更されたときに、結果リストのスクロール位置を一番上に戻す

--- MainApp/MainWindow.xaml.cs
+++ MainApp/MainWindow.xaml.cs
@@ -24,5 +24,17 @@ namespace MainApp
         {
             InitializeComponent();
         }
+
+        private void ListView_TargetUpdated(object sender, DataTransferEventArgs e)
+        {
+            var listView = sender as ListView;
+            if (listView == null || listView.Items == null || listView.Items.Count < 1)
+            {
+                return;
+            }
+
+            listView.Items.MoveCurrentToFirst();
+            listView.ScrollIntoView(listView.Items.CurrentItem);
+        }
     }
 }
