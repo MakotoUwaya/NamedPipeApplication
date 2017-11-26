98692e918e90585d69c34f58dbe8f8046a24ec75
Makoto Uwaya <makoto.uwaya@gmail.com>
2017-11-26 17:14:48 +0900

    文字列中に<BR>または<br>が含まれていたら改行するように
    - Converterを追加

---
+++ MainApp/Converter/TextNewLineConverter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Globalization;
+using System.Text.RegularExpressions;
+using System.Windows.Data;
+
+namespace MainApp.Converter
+{
+    public class TextNewLineConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var text = value as string;
+            if (text == null)
+            {
+                return value;
+            }
+
+            return Regex.Replace(text, "<[Bb][Rr]>", "\n");
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
